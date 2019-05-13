03ac2231bf772abf90bbaca11a2c7021b2ff6c0c
LukasKodym <lukasz.kodym@gmail.com>
2019-05-13 17:10:46 +0200

    preparing to use builder pattern, end 2.2

--- src/main/java/pl/lukas/App.java
+++ src/main/java/pl/lukas/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
+        new House("ul. Kursowa",2,3,12);
+        // new House .setAddress .setFloorNumber .setDoorsNumber . setWindowNumber
     }
-
 }
--- src/main/java/pl/lukas/House.java
+++ src/main/java/pl/lukas/House.java
@@ -1,4 +1,16 @@
 package pl.lukas;
 
 public class House {
+
+    private String address;
+    private Integer floorsNumber;
+    private Integer doorsNumber;
+    private Integer windowsNumber;
+
+    public House(String address, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
+        this.address = address;
+        this.floorsNumber = floorsNumber;
+        this.doorsNumber = doorsNumber;
+        this.windowsNumber = windowsNumber;
+    }
 }
